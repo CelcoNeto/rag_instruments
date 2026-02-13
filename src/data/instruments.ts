11cbd3bb43102fec9da2eb8d8451268d50b61d8d
CelcoNeto <celcoroxa@gmail.com>
2026-02-13 18:55:04 -0300

    refact: change texto to text

--- src/data/instruments.ts
+++ src/data/instruments.ts
@@ -1,22 +1,22 @@
 export const INSTRUMENTS_LIST = [
   {
     id: 1,
-    texto:
+    text:
       "A guitarra elétrica utiliza captadores magnéticos e é amplificada eletronicamente.",
   },
   {
     id: 2,
-    texto:
+    text:
       "O violão acústico produz som através da vibração das cordas em uma caixa de ressonância.",
   },
   {
     id: 3,
-    texto:
+    text:
       "O baixo elétrico possui frequências graves e normalmente quatro cordas.",
   },
   {
     id: 4,
-    texto:
+    text:
       "A bateria é um instrumento de percussão composto por tambores e pratos.",
   },
 ];
